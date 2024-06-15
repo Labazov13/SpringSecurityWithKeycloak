4a779adaadf6c3beb1adabe3315f2e05a509947c
labazov13 <labazopelastra@gmail.com>
2024-06-16 00:17:33 +0700

    Fix code

--- import-client-scope.sh
+++ import-client-scope.sh
@@ -1,4 +1,3 @@
 chmod +x /opt/keycloak/import-client-scope.sh
 
-sleep 20
 /opt/keycloak/bin/kcadm.sh config credentials --server http://keycloak:8080 --realm master --user $KEYCLOAK_ADMIN --password $KEYCLOAK_ADMIN_PASSWORD
